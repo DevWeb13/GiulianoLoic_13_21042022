022b8c6907f4bf9b83ab61c8264ed4eb3390a630
DevWeb13 <giuliano.loic@sfr.fr>
2022-04-21 22:12:11 +0200

    build: Create Routes...

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
@@ -8,7 +9,13 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/user/login" element={<SignIn />} />
+        {/* <Route path="*" element={<Error />} /> */}
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
