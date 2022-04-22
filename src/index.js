c685f4bcac9d183efb721f716b26c72e7b06c97a
DevWeb13 <giuliano.loic@sfr.fr>
2022-04-22 13:41:59 +0200

    feat: (index.js) : <Provider store={store}> and import store

--- src/index.js
+++ src/index.js
@@ -5,18 +5,22 @@ import "./index.css";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import User from "./pages/User";
+import { Provider } from "react-redux";
+import store from "./utils/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<SignIn />} />
-        <Route path="/user" element={<User />} />
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/user" element={<User />} />
 
-        {/* <Route path="*" element={<Error />} /> */}
-      </Routes>
-    </Router>
+          {/* <Route path="*" element={<Error />} /> */}
+        </Routes>
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
