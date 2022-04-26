4656a59823de05423ca73ce6736596da95300169
DevWeb13 <giuliano.loic@sfr.fr>
2022-04-26 10:43:19 +0200

    feat: Rename auth.js into login.js
    Create slice, actions ans selector

---
+++ src/features/login.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const loginSlice = createSlice({
+  name: "login",
+  initialState: {
+    login: null,
+  },
+  reducers: {
+    userLogin: (state, action) => {
+      state.login = action.payload;
+    },
+    userLogout: (state, action) => {
+      state.login = null;
+    },
+  },
+});
+
+export const { userLogin, userLogout } = loginSlice.actions;
+
+export const selectLogin = (state) => state.login;
+
+export default loginSlice.reducer;
