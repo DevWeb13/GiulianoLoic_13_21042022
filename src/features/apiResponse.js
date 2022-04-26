fc7db092f86d647907a254b18185ad852cee96b0
DevWeb13 <giuliano.loic@sfr.fr>
2022-04-26 11:59:16 +0200

    feat: (apiResponse.js): Create slice, actions and selector

---
+++ src/features/apiResponse.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const apiResponseSlice = createSlice({
+  name: "apiResponse",
+  initialState: {
+    apiResponse: null,
+  },
+  reducers: {
+    apiResponseLogin: (state, action) => {
+      state.apiResponse = action.payload;
+    },
+    apiResponseLogout: (state, action) => {
+      state.apiResponse = null;
+    },
+  },
+});
+
+export const { apiResponseLogin, apiResponseLogout } = apiResponseSlice.actions;
+
+export const selectApiResponse = (state) => state.apiResponse;
+
+export default apiResponseSlice.reducer;
