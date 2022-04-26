b3a0c8bd1a54f2d7e34f352d7b96f356ff34baee
DevWeb13 <giuliano.loic@sfr.fr>
2022-04-26 10:46:48 +0200

    feat: (store.js): Connect loginReducer

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/auth";
+import loginReducer from "../features/login";
 
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    login: loginReducer,
   },
 });
 
