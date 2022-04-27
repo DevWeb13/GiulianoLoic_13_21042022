c74f65b8c1ca4993a20c7eb35cccafaeebba1d35
DevWeb13 <giuliano.loic@sfr.fr>
2022-04-27 08:17:25 +0200

    feat: Create App.jsx
    Move router in App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
+import SignIn from "./pages/SignIn/SignIn";
+import User from "./pages/User/User";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/user" element={<User />} />
+
+        {/* <Route path="*" element={<Error />} /> */}
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import "./index.css";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Home from "./pages/Home/Home";
-import SignIn from "./pages/SignIn/SignIn";
-import User from "./pages/User/User";
+import App from "./App";
 import { Provider } from "react-redux";
 import store from "./utils/store";
 
@@ -14,17 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<SignIn />} />
-          <Route path="/user" element={<User />} />
-
-          {/* <Route path="*" element={<Error />} /> */}
-        </Routes>
-        <Footer />
-      </Router>
+      <App />
     </Provider>
   </React.StrictMode>
 );
